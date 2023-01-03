78733fa71719721f1d8e9470b341651463e896c5
JashminaThangarajah <121014310+JashminaThangarajah@users.noreply.github.com>
2023-01-03 14:04:24 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -2,6 +2,6 @@ class sum{
     public static void main(String args[]){
         int a = 6;
         int b= 4;
-        System.out.println(a+b);
+        System.out.println(a+b);//summation
     }
 }
